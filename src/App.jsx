a4e7bd9167063048bebbf2be98152b22a167bd4e
muhmed Sidin <129341811+Muhdsidin@users.noreply.github.com>
2023-11-22 22:17:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
+// App.js
 
-import { Route, Routes, useLocation, useParams } from "react-router-dom"
-import Header from "./components/Header/Header"
-
-import Home from './Pages/Home'
+import { Route, Routes, useLocation } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from './Pages/Home';
 import Buildings from "./Pages/AddBuilding";
-import Room from './Pages/AddRooms'
+import Room from './Pages/AddRooms';
+
 function App() {
   const location = useLocation();
-  console.log(location.pathname);
-  const shouldRenderHeader = location.pathname === "/real/admin" || location.pathname === "/upload-rooms" || location.pathname === "/upload-building" ;
-
-  
+  const shouldRenderHeader = location.pathname === "/real/admin" || location.pathname === "/upload-rooms" || location.pathname === "/upload-building";
 
   return (
     <div>
       {shouldRenderHeader && <Header />}
       <Routes>
         <Route path="/room" element={<Room />} />
-        <Route path="/buildings" element={<Buildings/>}/>
-        <Route path="/" element={<Home/>} />
-        
-
-
-
+        <Route path="/buildings" element={<Buildings />} />
+        <Route path="/building" element={<Building />} /> {/* Add this line */}
+        <Route path="/" element={<Home />} />
       </Routes>
-
-    
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
