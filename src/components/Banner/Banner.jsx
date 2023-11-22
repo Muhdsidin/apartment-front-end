a7e4570d2b19c50ed24cd62bf9f61a9a1f1ffdb3
razal <razalabdulla285@gmail.com>
2023-11-22 14:50:20 +0530

    after added buildings upload form

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import './Banner.css'
+import './Banner.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function Banner() {
+  const style={
+    box3:{
+      color: '#FFF',
+      transition: 'all 0.5s',
+      position: 'relative',
+
+    }
+
+  }
   return (
     <div>
+     <Container>
+      <Row className="justify-content-center">
+      <a href="/buildings" className='box-3 btn btn-secondary' style={{ marginTop: '4em',width:'10em'
+    }} >Upload Buildings</a>
+      
+ 
+      </Row>
+      
+      <Row className="justify-content-center">
+      <a href="/room" className='box-3 btn btn-secondary' style={{ marginTop: '1em',width:'10em' }}>Upload Rooms</a>
+      </Row>
+     </Container>
      
      
-     <a href="/Buildings" className='upload-builings-button btn btn-primary' >Upload Buildings</a>
+   
     </div>
+  
   )
 }
 export default Banner
