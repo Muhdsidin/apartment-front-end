427172f551e3250c8c182ebf9757fc62d4872b2e
Muhdsidin <officialzedro@gmail.com>
2024-01-02 23:19:42 +0530

    search added

--- src/components/ViewTenent/ViewTenent.jsx
+++ src/components/ViewTenent/ViewTenent.jsx
@@ -4,6 +4,9 @@ import './ViewTenent.css'
 import axios from 'axios'
 function ViewTenent() {
   const [data , setData] = useState([])
+  const [input , setInput ] = useState("")
+  const [searchdata , setSearchData] = useState()
+
   const fetchBooked =async ()=>{
     const response = await axios("https://apartment-one.vercel.app/get-all-book")
     console.log(response.data)
@@ -23,10 +26,22 @@ function ViewTenent() {
     })
     setData(response.data)
   }
+
+  const search = async()=>{
+    const response = await axios(`http://localhost:3000/${input}`)
+    console.log(response.data)
+    setSearchData(response.data)
+    setInput("")
+  }
+
+  
   return (
     <div className='table-main'>
       
     <Container style={{marginTop:'11em'}}>
+
+      <input type="text" onChange={(e)=> setInput(e.target.value)} />
+      <button onClick={search} value={input}> Search</button>
    
     <table className="table">
   <thead>
@@ -44,7 +59,25 @@ function ViewTenent() {
     </tr>
   </thead>
   <tbody>
-   {data.map((val)=>(
+
+
+    {searchdata?   <tr>
+     <th scope="row">1</th>
+     <td>{searchdata.name}</td>
+     <td>{searchdata.address}</td>
+     <td>{searchdata.country}</td>
+     <td>{searchdata.state}</td>
+     
+     <td>{searchdata.from}</td>
+     <td>{searchdata.to}</td>
+     <td>{searchdata.total}</td>
+     
+    <td><a href={`/edittenent/${searchdata._id}`} className='btn btn-primary'>Renew</a></td>
+    <td><button className='btn btn-danger' onClick={()=>terminateTannent(searchdata._id)}>Terminate</button></td> 
+   </tr>
+    :  
+    
+    data.map((val)=>(
      <tr>
      <th scope="row">1</th>
      <td>{val.name}</td>
@@ -59,7 +92,8 @@ function ViewTenent() {
     <td><a href={`/edittenent/${val._id}`} className='btn btn-primary'>Renew</a></td>
     <td><button className='btn btn-danger' onClick={()=>terminateTannent(val._id)}>Terminate</button></td> 
    </tr>
-   ))}
+   )) }
+
    
   </tbody>
 </table>
