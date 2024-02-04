40ba61aede8ed96c2b7d569f83f6c364789969f7
razal <razalabdulla285@gmail.com>
2024-02-04 12:58:33 +0530

    css

--- src/components/ViewTenent/ViewTenent.jsx
+++ src/components/ViewTenent/ViewTenent.jsx
@@ -39,9 +39,19 @@ function ViewTenent() {
     <div className='table-main'>
       
     <Container style={{marginTop:'11em'}}>
-
-      <input type="text" onChange={(e)=> setInput(e.target.value)} />
-      <button onClick={search} value={input}> Search</button>
+    <div class="input-group">
+  <div class="form-outline" data-mdb-input-init>
+    <input type="search" id="form1" class="form-control" placeholder='search' onChange={(e)=> setInput(e.target.value)}/>
+  
+  </div>
+  <button type="button" class="btn btn-primary" data-mdb-ripple-init  onClick={search} value={input}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16" >
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+</svg>
+  </button>
+</div>
+      {/* <input type="text" onChange={(e)=> setInput(e.target.value)} />
+      <button onClick={search} value={input}> Search</button> */}
    
     <table className="table">
   <thead>
@@ -89,8 +99,13 @@ function ViewTenent() {
      <td>{val.to}</td>
      <td>{val.total}</td>
      
-    <td><a href={`/edittenent/${val._id}`} className='btn btn-primary'>Renew</a></td>
-    <td><button className='btn btn-danger' onClick={()=>terminateTannent(val._id)}>Terminate</button></td> 
+    <td><a href={`/edittenent/${val._id}`} className=''><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
+</svg></a></td>
+    <td><svg xmlns="http://www.w3.org/2000/svg"  onClick={()=>terminateTannent(val._id)} width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg></td> 
    </tr>
    )) }
 
