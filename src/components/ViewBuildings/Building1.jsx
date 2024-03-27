cc851f6e5e3d71e61eb281af9de060f1d4cdc790
Muhdsidin <officialzedro@gmail.com>
2024-03-27 22:43:20 +0530

    testing

--- src/components/ViewBuildings/Building1.jsx
+++ src/components/ViewBuildings/Building1.jsx
@@ -22,7 +22,7 @@ function Building1() {
     getRoom();
   }, []);
 
-  const editRoom = async (roomId)=>{
+  const DeleteRoom = async (roomId)=>{
     const response = await axios("https://apartment-one.vercel.app/delete-room",{
       method:"POST",
       data:{
@@ -46,7 +46,7 @@ function Building1() {
           <div class="card-footer">
             <span class="text-title">{val.prize}/-AED <p className="per-day"> per day</p></span>
            {val.book ? (
- <div class="card-button">
+ <div class="card-button" > 
    <svg class="svg-icon" viewBox="0 0 20 20">
    <svg class="svg-icon" viewBox="0 0 20 20">
                     <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
@@ -81,7 +81,7 @@ function Building1() {
 </svg></a>
             </>
           ) : (
-            <>
+            <div onClick={()=> DeleteRoom(val._id)}>
               <a className="delete-room" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
@@ -89,7 +89,7 @@ function Building1() {
               <a href={`/roomedit/${val._id}`} className="delete-room" disabled><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
   <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
 </svg></a>
-            </>
+            </div>
           )}
         </div>
         
