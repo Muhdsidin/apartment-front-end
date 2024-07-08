6e52b97bff29eeb419288c2d112eac0ce3845820
razalabdulla <razalabdulla285@gmail.com>
2024-07-08 10:51:38 +0530

    form

--- src/components/Login/Adminlogin.jsx
+++ src/components/Login/Adminlogin.jsx
@@ -4,30 +4,27 @@ import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 function Adminlogin() {
-
-
-
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState()
 
     return (
         <Container style={{ marginTop: '7em' }}>
             <Form>
-                <h2 style={{ textAlign: 'center',color:"white" }}>Login To Continue</h2>
+                <h2 style={{ textAlign: 'center', color: "white" }}>Login To Continue</h2>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+                    <Form.Label >Email address</Form.Label>
+                    <Form.Control onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+                    <br />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
                 </Form.Group>
                 {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group> */}
-                <Button variant="" type="submit" style={{float:"right",color:"black",background:'white'}}>
+                <Button variant="" type="submit" style={{ float: "right", color: "black", background: 'white' }}>
                     Submit
                 </Button>
             </Form>
